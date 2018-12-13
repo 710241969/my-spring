27dd1fb893010f1360b72c24a60710917b02c64c
unknown <710241969@qq.com>
2018-12-14 01:40:53 +0800

    完善注释

--- src/main/java/com/demo/mvcframework/servlet/DispatchServlet.java
+++ src/main/java/com/demo/mvcframework/servlet/DispatchServlet.java
@@ -2,7 +2,6 @@ package com.demo.mvcframework.servlet;
 
 import com.demo.mvcframework.annotation.*;
 import com.demo.utils.StringUtil;
-import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -14,7 +13,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.*;
@@ -22,10 +20,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class DispatchServlet extends HttpServlet {
-    private Properties contextConfig = new Properties();
-    private List<String> classNameList = new ArrayList<>();
-    private Map<String, Object> ioc = new HashMap<>(); // ioc.put(beanName, instance)
-    private List<Handler> handlerList = new ArrayList<>();
+    private Properties contextConfig = new Properties(); // 读取的配置内容
+    private List<String> classNameList = new ArrayList<>(); // 扫描到的类名列表
+    private Map<String, Object> ioc = new HashMap<>(); // 传说中的 IOC 容器其实就是个 Map， ioc.put(beanName, instance)
+    private List<Handler> handlerList = new ArrayList<>(); // 自己实现的 Handler 对象列表，存放处理请求的 Handler 信息
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -90,13 +88,16 @@ public class DispatchServlet extends HttpServlet {
      */
     @Override
     public void init(ServletConfig config) throws ServletException {
-        // 加载配置文件，从 init 方法的参数中，我们可以拿到主配置文件的路径
+        // 加载配置文件，从 doLoadConfig 方法中，我们获取配置的内容
+        // 其中 ServletConfig 的 getInitParameter 方法可以拿到 web.xml 中这个 servlet 的指定属性的值
+        // 所以这里拿 contextConfigLocation 这个属性的值，结果是 application.properties
         doLoadConfig(config.getInitParameter("contextConfigLocation"));
 
-        // 扫描相关类
+        // 扫描指定包内的类
+        // contextConfig 在上面的 doLoadConfig 方法中得到初始化
         doScan(contextConfig.getProperty("scanPackage"));
 
-        // 初始化相关联的类，实例化饼放入 IOC 容器中
+        // 构造 IOC 容器。初始化相关联的类，实例化并放入 IOC 容器中
         doInstance();
 
         // 自动化依赖注入
@@ -104,7 +105,6 @@ public class DispatchServlet extends HttpServlet {
 
         // 初始化 HandlerMapping
         initHandlerMapping();
-
     }
 
     /**
@@ -171,28 +171,32 @@ public class DispatchServlet extends HttpServlet {
 
     /**
      * 实例化相关的类，并放入 IOC 容器中
-     * 只有加了 Controller 和 Service 注解的类才实例化
-     * 容器的 key 默认是类名首字母小写
+     * 只有加了 Controller 和 Service 注解的类才需要实例化，应当以单例模式实例化
+     * IOC 容器的 key 默认是类名首字母小写
      */
     private void doInstance() {
-        // 判断有没有扫描到
+        // 判断在上一步中有没有扫描到类，没有直接退出
         if (classNameList.isEmpty()) {
             return;
         }
         try {
             for (String item : classNameList) {
-                // 拿到指定类名的class对象，进行反射
+                // 拿到指定类名的class对象，进行反射，只有加了 Controller 和 Service 注解的类才需要处理
                 Class<?> clazz = Class.forName(item);
-                // isAnnotationPresent 判断该类是否存在该注解，然后通过反射实例化此类
+                // isAnnotationPresent 方法判断该类是否存在指定注解
                 if (clazz.isAnnotationPresent(Controller.class)) {
-                    String key = StringUtil.lowerCaseFirst(clazz.getSimpleName()); // 获取底层类名，转换为首字母小写
+                    // 获取底层类名，转换为首字母小写
+                    // 注意 getName 和 getSimpleName 方法的区别
+                    String key = StringUtil.lowerCaseFirst(clazz.getSimpleName()); // StringUtil 是个自己写的简单工具类而已
+                    // 通过反射 newInstance 方法实例化此类，放入 IOC 容器中
                     ioc.put(key, clazz.newInstance());
                 } else if (clazz.isAnnotationPresent(Service.class)) {
                     Service service = clazz.getAnnotation(Service.class);
-                    String beanName = service.value();
                     Object instance = clazz.newInstance();
+                    // Service 可能使用指定的名字，所以比上面多一点处理
+                    String beanName = service.value();
                     if ("".equals(beanName.trim())) {
-                        // 没有指定名字则使用类名首字母小写
+                        // 没有指定名字则使用默认的类名首字母小写
                         beanName = StringUtil.lowerCaseFirst(clazz.getSimpleName());
                     }
                     ioc.put(beanName, instance);
@@ -209,7 +213,7 @@ public class DispatchServlet extends HttpServlet {
     }
 
     /**
-     * 递归扫描指定包下的全部 class
+     * 扫描指定包下的全部 class ,为 classNameList 加入数据
      *
      * @param scanPackage
      */
@@ -217,27 +221,52 @@ public class DispatchServlet extends HttpServlet {
         // 拿到包路径
         URL url = this.getClass().getClassLoader().getResource("/" + scanPackage.replace(".", "/"));
         File classDir = new File(url.getFile());
-        // 递归遍历整个包
-        for (File item : classDir.listFiles()) {
+        // 如果指定扫描的不是包而是文件，直接加入列表返回，或者抛出异常也行
+        if (classDir.isFile()) {
+            classNameList.add(scanPackage);
+            return;
+        }
+        // 因为包下面可能还有很多包，所以需要不断遍历
+        // 方法之一，递归遍历整个包，代码被我注释掉了，但是我不喜欢递归。能用循环的我都用循环
+/*        for (File item : classDir.listFiles()) {
             if (item.isDirectory()) {
                 doScan(scanPackage + "." + item.getName());
             } else {
                 String className = (scanPackage + "." + item.getName()).replace(".class", "");
                 classNameList.add(className);
             }
+        }*/
+
+        // 使用循环代替遍历，就是优先搜索，这里是宽度优先搜索
+        List<String> pathList = new ArrayList<>();
+        List<File> dirList = new ArrayList<>();
+        pathList.add(scanPackage);
+        dirList.add(classDir);
+        while (!dirList.isEmpty()) {
+            File dirItem = dirList.remove(0);
+            String pathItem = pathList.remove(0);
+            for (File fileItem : dirItem.listFiles()) {
+                if (fileItem.isDirectory()) {
+                    dirList.add(fileItem);
+                    pathList.add(pathItem + "." + fileItem.getName());
+                    continue;
+                }
+                String className = (pathItem + "." + fileItem.getName()).replace(".class", "");
+                classNameList.add(className);
+            }
         }
     }
 
     /**
-     * 从能够读取到配置文件中的信息，读取到Properties对象中
+     * 从能够读取到 web.xml 配置文件中的信息，读取到 Properties 对象中
      *
      * @param contextConfigLocation
      */
     private void doLoadConfig(String contextConfigLocation) {
-        // 拿到 Spring 配置文件路径 读取文件中所有内容
+        // 从传入的配置文件名，读取文件中所有配置内容
         InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(contextConfigLocation);
         try {
-            contextConfig.load(inputStream);
+            contextConfig.load(inputStream); // 赋值给 contextConfig
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
